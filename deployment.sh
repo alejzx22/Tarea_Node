f052c7b0f0c5aaadfe9b90031fce535104df71f2
Alejandro Alfaro <alejalfaro@estudiantec.cr>
2024-05-03 14:45:43 -0600

    Update response message in index.js and restart server in deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -6,6 +6,7 @@ cd /home/alej/Documentos/Tarea_Node
 # Check if there are changes in the deployment branch and if so, deploy the changes
 if [ -n "$(git diff --name-only origin/Deployment)" ]; then
   git pull origin Deployment
+  echo "Restarting server."
   systemctl restart hello_world.service
 fi
 
