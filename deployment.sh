7727b41223b4d829fe5c816f579669dd8b342abc
Alejandro Alfaro <alejalfaro@estudiantec.cr>
2024-05-03 14:36:03 -0600

    Refactor response message in index.js and add Linux service setup for Node.js server and deployment

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Change to the project directory
+cd /home/alej/Documentos/Tarea_Node
+
 # Check if there are changes in the deployment branch and if so, deploy the changes
 if [ -n "$(git diff --name-only origin/Deployment)" ]; then
   git pull origin Deployment
