7f4838a726dabfd0146ba0c715d1ec59679614c2
Alejandro Alfaro <alejalfaro@estudiantec.cr>
2024-05-17 17:50:13 -0600

    chore: Update deployment script to restart service with sudo

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cd /home/alej/Downloads/Tarea_Node-main
+
+git fetch origin
+
 # Check if there are changes in the deployment branch and if so, deploy the changes
 if [ -n "$(git diff --name-only origin/Deployment)" ]; then
   git pull origin Deployment
-  systemctl restart hello_world.service
+  sudo systemctl restart hello_world.service
 fi
-
